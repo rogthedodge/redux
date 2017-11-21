a59cb181934b4d12b417623e30f3795bb176a5ca
Willrholmes01 <will.r.holmes@gmail.com>
2017-11-21 18:08:32 +0000

    Added basic test framework

--- test/test_db.py
+++ test/test_db.py
@@ -1,8 +1,8 @@
 import psycopg2
 from unittest import TestCase
 
-from source.config import config
-from source.create_DB import create_tables
+from ..source.config import config
+from ..source.create_DB import create_tables
 
 
 class CreateDBTestCase(TestCase):
@@ -15,15 +15,33 @@ class CreateDBTestCase(TestCase):
     def setUp(self):
         self.params = config()
         self.conn = psycopg2.connect(**params)
-        self.cursor = conn.cursor()
+        self.cur = conn.cursor()
 
-    def test_create_tables(self):
-        """Unit test for create_tables function"""
+        # run create tables
+        create_tables()
+
+    def test_database_created(self):
+        """Unit test to check redux database is created"""
         # check redux database has been created
         sql = "SELECT datname FROM pg_database WHERE datistemplate = false;"
-        dbs = self.cursor.execute(sql)
+        self.cur.execute(sql)
+        dbs = self.cur.fetchall()
         print(dbs)
 
-        #
+        # assert database is there
+        self.assertIn('redux', dbs)
+
+    def test_persons_table(self):
+        """Unit test to check persons table is created correctly"""
+        sql = """
+                SELECT column_name, data_type, character_maximum_length
+                FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = persons; 
+              """
+        self.cur.execute(sql)
+        table_info = self.cur.fetchall()
+        print(table_info)
+
+
+
 
 
